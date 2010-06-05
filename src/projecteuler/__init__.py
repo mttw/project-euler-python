6993c6696cf6ea975c751a7f6715fe467e694528
Matthias <Matthias@Shrike>
2010-06-05 11:11:02 +0200

    commited util methods

--- src/projecteuler/__init__.py
+++ src/projecteuler/__init__.py
@@ -17,6 +17,7 @@ def find_first_n(f, n, seq):
             if len(l) == n:
                 return l
 
+
 def find_first(f, seq):
     """Return first item in sequence where f(item) == True."""
     for item in seq:
